2f181ba0a001bb52f1e0fe14d66fe60ec70d4546
Daniil Novikov <dannovik30@gmail.com>
2022-04-15 14:15:40 +0200

    Update index.js

--- containers/index/index.js
+++ containers/index/index.js
@@ -2,7 +2,6 @@ export { default as Spotlight } from './Spotlight/Spotlight';
 export { default as AboutUs } from './AboutUs/AboutUs';
 export { default as AboutCollection } from './AboutCollection/AboutCollection';
 export { default as Road } from './Road/Road';
-export { default as Rope } from './Rope/Rope';
 export { default as Wave } from './Wave/Wave';
 export { default as NftPreview } from './NftPreview/NftPreview';
 export { default as GetAnimalsBack } from './GetAnimalsBack/GetAnimalsBack';
