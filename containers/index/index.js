effd179bb687bc06438ea577b387a9923cb64a27
daniil <daniil@clean.email>
2022-04-17 11:49:09 +0200

    fix

--- containers/index/index.js
+++ containers/index/index.js
@@ -5,5 +5,5 @@ export { default as Road } from './Road/Road';
 export { default as Wave } from './Wave/Wave';
 export { default as NftPreview } from './NftPreview/NftPreview';
 export { default as GetAnimalsBack } from './GetAnimalsBack/GetAnimalsBack';
-export { default as Wallpapers } from './Wallpapers/Wallpapers';
+export { default as Wallpapers } from './Wallpapers/Wallpapers.vue';
 export { default as FlyingElephant } from './FlyingElephant/FlyingElephant';
